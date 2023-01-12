7c98b85adf70374b001d9a4884fba36b82545070
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-01-12 18:12:28 +0200

    Update routers.tf

--- pagerduty/routers.tf
+++ pagerduty/routers.tf
@@ -14,10 +14,19 @@ resource "pagerduty_event_orchestration_router" "router" {
     rule {
        label = "Events relating to Ace"
        condition {
-        expression = "event.summary matches part 'ace '"
+        expression = "event.summary matches part 'ace\\W'"
        }
        actions {
         route_to = pagerduty_service.Ace.id
+       }
+    }
+    rule {
+       label = "Events relating to Acer"
+       condition {
+        expression = "event.summary matches regex 'acer\\W'"
+       }
+       actions {
+        route_to = pagerduty_service.Acer.id
        }
     }
     rule {
