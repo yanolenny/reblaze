a63e0cfb52d4b2b2f4f1b14d7381e72c030eee96
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-02-14 18:21:44 +0200

    Update orchestrations.tf

--- pagerduty/orchestrations.tf
+++ pagerduty/orchestrations.tf
@@ -1,4 +1,3 @@
-resource "pagerduty_event_orchestration" "NewRelic" {
+data "pagerduty_event_orchestration" "NewRelic" {
   name = "NewRelic Monitoring Orchestration"
-  description = "Send events from NewRelic to list of services by PlanetID"
 }
