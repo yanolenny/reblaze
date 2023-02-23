a33f32253950a446cc5cbe41a98c0d3aedb24e64
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-02-23 17:46:26 +0200

    Update routers.tf

--- pagerduty/routers.tf
+++ pagerduty/routers.tf
@@ -5,7 +5,7 @@ resource "pagerduty_event_orchestration_router" "router" {
     rule {
       label = "Events relating to Ace"
       condition {
-        expression = "event.summary matches part 'ace$'"
+        expression = "event.summary matches part 'ace\\W'"
       }
       actions {
           route_to = pagerduty_service.ace.id
