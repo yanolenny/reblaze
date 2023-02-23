e3df20651a919aa00197c8b90d65c56de0f8ced5
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-02-23 16:41:16 +0200

    Update routers.tf

--- pagerduty/routers.tf
+++ pagerduty/routers.tf
@@ -3,40 +3,40 @@ resource "pagerduty_event_orchestration_router" "router" {
   set {
     id = "start"
     rule {
-     label = "Events relating to Ace"
-     condition {
-      expression = "event.summary matches part ''ace\\b''"
-     }
-     actions {
+      label = "Events relating to Ace"
+      condition {
+      expression = "event.summary matches part 'ace\\b'"
+      }
+      actions {
       route_to = pagerduty_service.ace.id
-     }
+      }
     }
     rule {
-     label = "Events relating to Adama"
-     condition {
-      expression = "event.summary matches part ''adama\\b''"
-     }
-     actions {
+      label = "Events relating to Adama"
+      condition {
+      expression = "event.summary matches part 'adama\\b'"
+      }
+      actions {
       route_to = pagerduty_service.adama.id
-     }
+      }
     }
     rule {
-     label = "Events relating to AdTorqueEdge"
-     condition {
-      expression = "event.summary matches part ''adtorqueedge\\b''"
-     }
-     actions {
+      label = "Events relating to AdTorqueEdge"
+      condition {
+      expression = "event.summary matches part 'adtorqueedge\\b'"
+      }
+      actions {
       route_to = pagerduty_service.adtorqueedge.id
-     }
+      }
     }
     rule {
-     label = "Events relating to AeroLines"
-     condition {
-      expression = "event.summary matches part ''aerolineas\\b''"
-     }
-     actions {
+      label = "Events relating to AeroLines"
+      condition {
+      expression = "event.summary matches part 'aerolineas\\b'"
+      }
+      actions {
       route_to = pagerduty_service.aerolineas.id
-     }
+      }
     }
   }
   catch_all {
