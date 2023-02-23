a9ca4905c31c996011dcad6d1dd62ba5d6fb8b62
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-02-23 14:17:43 +0200

    Update services.tf

--- pagerduty/services.tf
+++ pagerduty/services.tf
@@ -1,22 +1,22 @@
-resource "pagerduty_service" "Ace" {
+resource "pagerduty_service" "ace" {
   name = "Ace"
   description = "Ace (ace)"
   escalation_policy = data.pagerduty_escalation_policy.default.id
 }
 
-resource "pagerduty_service" "Adama" {
+resource "pagerduty_service" "adama" {
   name = "Adama"
   description = "Adama (adama)"
   escalation_policy = data.pagerduty_escalation_policy.default.id
 }
 
-resource "pagerduty_service" "AdTorqueEdge" {
+resource "pagerduty_service" "adtorqueedge" {
   name = "AdTorqueEdge.com"
   description = "AdTorqueEdge.com (adtorqueedge)"
   escalation_policy = data.pagerduty_escalation_policy.default.id
 }
 
-resource "pagerduty_service" "AeroLines" {
+resource "pagerduty_service" "aerolineas" {
   name = "AeroLines"
   description = "AeroLines (aerolineas)"
   escalation_policy = data.pagerduty_escalation_policy.default.id
