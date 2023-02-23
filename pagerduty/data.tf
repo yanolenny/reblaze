73bbbafd47460558250ea0d93184a9bbeb85c48a
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-02-23 10:38:34 +0200

    Update data.tf

--- pagerduty/data.tf
+++ pagerduty/data.tf
@@ -5,7 +5,3 @@ data "pagerduty_escalation_policy" "default"{
 data "pagerduty_escalation_policy" "code_red"{
   name = "Code Red"
 }
-
-data "pagerduty_event_orchestration" "NewRelic" {
-  name = "NewRelic Monitoring Orchestration"
-}
