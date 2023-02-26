2bc5736126d06417c72707b154880e2cd80d1b83
Yaniv Levi <yaniv@webiscope.com>
2023-02-26 16:36:34 +0200

    V2

--- pagerduty/routers.tf
+++ pagerduty/routers.tf
@@ -5,10 +5,37 @@ resource "pagerduty_event_orchestration_router" "router" {
     rule {
       label = "Events relating to Ace"
       condition {
-        expression = "event.summary matches part '\\''ace\\b'\\''"
+        expression = "event.summary matches regex 'ace\\\\b'"
       }
       actions {
           route_to = pagerduty_service.ace.id
+      }
+    }
+   rule {
+      label = "Events relating to Adama"
+      condition {
+        expression = "event.summary matches regex 'adama\\\\b'"
+      }
+      actions {
+          route_to = pagerduty_service.adama.id
+      }
+    }
+   rule {
+      label = "Events relating to AdTorqueEdge.com"
+      condition {
+        expression = "event.summary matches regex 'adtorqueedge\\\\b'"
+      }
+      actions {
+          route_to = pagerduty_service.adtorqueedge.id
+      }
+    }
+   rule {
+      label = "Events relating to AeroLines"
+      condition {
+        expression = "event.summary matches regex 'aerolineas\\\\b'"
+      }
+      actions {
+          route_to = pagerduty_service.aerolineas.id
       }
     }
   }
