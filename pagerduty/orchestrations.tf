0aa7b5fa8e1bcc84c9bc88290e1f047d353c8356
Yaniv Levi <yaniv@webiscope.com>
2023-03-01 11:44:36 +0200

    ssl_expiration

--- pagerduty/orchestrations.tf
+++ pagerduty/orchestrations.tf
@@ -2,3 +2,7 @@ resource "pagerduty_event_orchestration" "devops" {
   name = "Devops"
   description = "Send events from DevOps to a list of services"
 }
+resource "pagerduty_event_orchestration" "ssl_expiration" {
+  name = "SSL Expiration"
+  description = "Send events from SSL Expiration to a list of services"
+}
